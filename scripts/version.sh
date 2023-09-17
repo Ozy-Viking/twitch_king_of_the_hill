59e19398422cf754b2591309bbc27c753c868ccb
Ozy-Viking <admin@hankin.io>
2023-09-17 15:46:07 +1000

    subs rc3

---
+++ scripts/version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+koth_page=$(pwd)/website/index.html
+subs_page=$(pwd)/website/subs.html
+version=$1
+
+pages=($koth_page $subs_page)
+
+for page in "${pages[@]}"; do
+    echo $page;
+    if [[ -e $page ]]; then
+        sed -e "s/(version_tag)/($version)/" $page > $page
+    fi
+done
