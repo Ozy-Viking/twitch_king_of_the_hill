fcaa285cc81eb2671edecf2eca729399b96a792c
Ozy-Viking <admin@hankin.io>
2023-10-26 20:45:30 +1100

    fixed script

--- scripts/gh_pages.sh
+++ scripts/gh_pages.sh
@@ -5,6 +5,7 @@ set -x
 pages[0]=$(pwd)/website/index.html
 pages[1]=$(pwd)/website/subs.html
 pages[2]=$(pwd)/website/counter.html
+pages[3]=$(pwd)/website/testing.html
 
 for page in "${pages[@]}"; do
     echo $page;
