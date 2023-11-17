03534cc9077b5bd2249d99135a5cc49809b1f8f9
Ozy-Viking <admin@hankin.io>
2023-11-17 17:02:59 +1100

    bug fixes

--- scripts/gh_pages.sh
+++ scripts/gh_pages.sh
@@ -7,7 +7,7 @@ set -x
 for page in "${pages[@]}"; do
     echo $page;
     if [[ -e $page ]]; then
-        sed -e "s/src=\"\/static/src=\".\/static/" -e "s/href=\"\/static/href=\".\/static/" $page | tee $page.tmp
+        sed -e "s/src=\"\/static/src=\".\/static/" -e "s/href=\"\/static/href=\".\/static/" $page | tee $page.tmp 1>/dev/null  
         mv -f ${page}.tmp ${page}
     fi
 done
--- scripts/version.sh
+++ scripts/version.sh
@@ -9,7 +9,7 @@ version=$1
 for page in "${pages[@]}"; do
     echo $page;
     if [[ -e $page ]]; then
-        sed -e "s/([0-9a-zA-Z_.+-]*)/($version)/" $page | tee $page.tmp
+        sed -e "s/([0-9a-zA-Z_.+-]*)/($version)/" $page | tee $page.tmp 1>/dev/null  
         mv -f ${page}.tmp ${page}
     fi
 done
