6ee45ea083956803aebeb4ae2578db41b15c4817
Ozy-Viking <admin@hankin.io>
2023-11-18 15:53:18 +1100

    Fix: Updated update script
    Modified script to not require a message.

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 version="v0.1.0"
-# set -o errexit
+set -o errexit
 
-while getopts ':Vm:sat:p' OPTION; do
+while getopts 'Vmsat:p' OPTION; do
 case "$OPTION" in
   V )
     echo "$version"
@@ -27,7 +27,6 @@ case "$OPTION" in
 esac;
 done
 
-
 if [[ -n "$git_tag" ]]; then
   sed -i "s/\(image:.*:\).*$/\1${git_tag}/" ./docker-compose.yaml
   ${0%/*}/scripts/version.sh v${git_tag}
@@ -36,7 +35,13 @@ if [[ -n "$ADD" ]]; then
   git add ${0%/*}
 fi
 if [[ -n "$COMMIT" ]]; then
-  git commit -a --allow-empty -m "${msg}"
+echo "commit"
+  if [[ -n "$msg" ]]; then
+    git commit -a --allow-empty -m "${msg}"
+  else
+    echo "right place"
+    git commit -a --allow-empty
+  fi
 fi
 if [[ -n "$git_tag" ]]; then
   git tag v${git_tag}
